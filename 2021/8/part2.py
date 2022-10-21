181be960c16a9587cc530fdcfc5e8cd1397abff1
Jovier Jimenez <51138002+jimenezj8@users.noreply.github.com>
2022-10-21 09:57:39 -0700

    day8 part 2 wip

--- 2021/8/part2.py
+++ 2021/8/part2.py
@@ -1,4 +1,4 @@
-with open("input.txt") as file:
+with open("example1.txt") as file:
     data = [line.split(" | ") for line in file.read().split("\n") if line != ""]
 
 positions = {"t", "tr", "tl", "m", "br", "bl", "b"}
@@ -14,6 +14,18 @@ mapping = {
     8: positions.copy(),
     9: positions.difference({"bl"}),
 }
+
+def filter_clues(number: int, characters: set[str], clues: dict[int, set[str]]):
+    keep = mapping[number]
+    toss = positions.difference(keep)
+
+    return_clues = clues.copy()
+    for position in keep:
+        return_clues[position].intersection_update(characters)
+    for position in toss:
+        return_clues[position].difference_update(characters)
+
+    return return_clues
 
 total = 0
 for entry in data:
@@ -36,21 +48,23 @@ for entry in data:
         total += int(value)
         continue
 
-    all = signals + output
-
-    lengths = [len(val) for val in all]
+    all_sequences = signals + output
+    lengths = [len(val) for val in all_sequences]
 
     # 0, 6, 9 will all have len 6 and would be missing mid, top right, and bottom left
     # 1 has len 2 and has both right pieces
     # 2, 3, 5 all have len 5 and missing top right + bottom left, both left, and top right + bottom left
     # 4 has len 4
     # 7 has len 3
-    # 8 has len 7
+    # 8 has len 7 and provides no information to us
+
+    unique = [all_sequences[index] for index, length in enumerate(lengths) if length in [2, 3, 4]]
+
+    # first we filter the unique ones that give the most information
 
     clues = {loc: set(["a", "b", "c", "d", "e", "f", "g"]) for loc in positions}
-    for char_count in [2, 3, 4]:
+    for sequence, char_count in zip(unique, [len(string) for string in unique]):
         try:
-            chars = set([char for char in all[lengths.index(char_count)]])
             match char_count:
                 case 2:
                     key = 1
@@ -60,19 +74,42 @@ for entry in data:
                     key = 4
                 case _:
                     continue
-            keep = mapping[key]
-            toss = positions.difference(keep)
-
-            for pos in keep:
-                clues[pos].intersection_update(chars)
-            for pos in toss:
-                clues[pos].difference_update(chars)
+            clues = filter_clues(key, set([char for char in sequence]), clues)
         except ValueError:
             continue
 
-    length5 = [2, 3, 5]
-    length6 = [0, 6, 9]
-    # TODO: find how to discern which len(5) values correspond to which number
+    potential_solution = clues.copy()
+    print(potential_solution)
+    for sequence, num_chars in zip(all_sequences, lengths):
+        if num_chars == 5:
+            for key in [2, 3, 5]:
+                next_attempt = filter_clues(key, set([char for char in sequence]), potential_solution)
+
+                if all([len(possibilities) == 1 for possibilities in next_attempt.values()]):
+                    potential_solution = next_attempt.copy()
+                    break
+
+                elif set() not in next_attempt.values():
+                    potential_solution = next_attempt.copy()
+
+        elif num_chars == 6:
+            for key in [0, 6, 9]:
+                next_attempt = filter_clues(key, set([char for char in sequence]), potential_solution)
+
+                if all([len(possibilities) == 1 for possibilities in next_attempt.values()]):
+                    potential_solution = next_attempt.copy()
+                    break
+
+                elif set() not in next_attempt.values():
+                    potential_solution = next_attempt.copy()
+
+        else:
+            continue
+
+        if all([len(possibilities) == 1 for possibilities in potential_solution.values()]):
+            break
+
+print(potential_solution)
 
 
 print(f"The sum of the output is {total}")
