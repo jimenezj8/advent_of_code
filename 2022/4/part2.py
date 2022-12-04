dd26b64dfd704c59bd6466d8229c2864afa9adb8
Jovier A. Jiménez <jthree8@gmail.com>
2022-12-04 14:38:30 -0800

    formatting

--- 2022/4/part2.py
+++ 2022/4/part2.py
@@ -1,5 +1,5 @@
-with open('./input.txt') as file:
-    pairs = [line.strip().split(',') for line in file.readlines()]
+with open("./input.txt") as file:
+    pairs = [line.strip().split(",") for line in file.readlines()]
 
 
 overlaps = 0
@@ -7,8 +7,12 @@ overlaps = 0
 for pair in pairs:
     first, second = pair
 
-    first = {i for i in range(int(first.split('-')[0]), int(first.split('-')[1]) + 1, 1)}
-    second = {i for i in range(int(second.split('-')[0]), int(second.split('-')[1]) + 1, 1)}
+    first = {
+        i for i in range(int(first.split("-")[0]), int(first.split("-")[1]) + 1, 1)
+    }
+    second = {
+        i for i in range(int(second.split("-")[0]), int(second.split("-")[1]) + 1, 1)
+    }
 
     if first.intersection(second) or second.intersection(first):
         overlaps += 1
