55e200c4a96c00ee69044d50b62082adddfeddf7
Jovier A. Jiménez <jthree8@gmail.com>
2024-02-19 16:37:40 -0800

    optimize priority calculation year2022 day03

--- rust/src/year2022/day03/mod.rs
+++ rust/src/year2022/day03/mod.rs
@@ -5,42 +5,37 @@ fn parse(contents: &str) -> Vec<&str> {
     contents.lines().collect()
 }
 
-fn part1(sacks: &Vec<&str>, priorities: &Vec<char>) -> u32 {
+fn priority_from_char(input: char) -> u32 {
+    let value = input as u32;
+    let lower_ref = 'a' as u32;
+    let upper_ref = 'A' as u32;
+
+    if value >= lower_ref {
+        value - lower_ref + 1
+    } else {
+        value - upper_ref + 27
+    }
+}
+
+fn part1(sacks: &Vec<&str>) -> u32 {
     sacks
         .iter()
         .map(|sack| sack.split_at(sack.len() / 2))
-        .map(|(left, right)| {
-            right
-                .chars()
-                .filter(|item| left.contains(*item))
-                .map(|item| {
-                    priorities
-                        .iter()
-                        .position(|&priority| priority == item)
-                        .unwrap()
-                })
-                .collect::<Vec<_>>()
-        })
-        .map(|sack| (sack[0] + 1) as u32)
+        .map(|sack| sack.0.chars().find(|item| sack.1.contains(*item)).unwrap())
+        .map(|shared_item| priority_from_char(shared_item))
         .sum()
 }
 
-fn part2(sacks: &Vec<&str>, priorities: &Vec<char>) -> u32 {
+fn part2(sacks: &Vec<&str>) -> u32 {
     sacks
         .chunks(3)
         .map(|group| {
             group[0]
                 .chars()
-                .filter(|item| group[1].contains(*item) && group[2].contains(*item))
-                .map(|item| {
-                    priorities
-                        .iter()
-                        .position(|&priority| priority == item)
-                        .unwrap()
-                })
-                .collect::<Vec<_>>()
+                .find(|item| group[1].contains(*item) && group[2].contains(*item))
+                .unwrap()
         })
-        .map(|group_priority| (group_priority[0] + 1) as u32)
+        .map(|shared_item| priority_from_char(shared_item))
         .sum()
 }
 
@@ -50,14 +45,10 @@ pub fn solve() -> Solution<u32, u32> {
     let contents = include_str!("./input.txt");
     let sacks = parse(contents);
 
-    let priorities: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        .chars()
-        .collect();
-
     Solution {
         day: 3,
-        part1: part1(&sacks, &priorities),
-        part2: part2(&sacks, &priorities),
+        part1: part1(&sacks),
+        part2: part2(&sacks),
         runtime: SystemTime::now().duration_since(start).unwrap(),
     }
 }
